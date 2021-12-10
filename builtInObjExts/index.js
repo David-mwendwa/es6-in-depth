8deae2530f13529467030a14e25a2698c65ebac0
David Mwendwa <klatzdavie19@gmail.com>
2021-12-10 18:39:14 +0300

    es6(6): extensions of built-in-objects

---
+++ builtInObjExts/index.js
@@ -0,0 +1,59 @@
+/*********Object */
+// let obj1 = {
+//   a: 1
+// }
+// let obj2 = {
+//   b: 2
+// }
+// let obj = Object.assign(obj1, obj2)
+// let objj = {...obj1, ...obj2}
+// console.log(obj)
+// console.log(objj)
+
+
+/*******Strings */
+// let name = "David Mwendwa"
+// console.log(name.startsWith("D")) //case-sensitive
+// console.log(name.endsWith("a")) //case-sensitive
+// console.log(name.includes(" Mwen")) //case-sensitive
+
+
+/*******Number Object */
+// let number = 78
+// console.log(Number.isNaN(NaN))
+// console.log(Number.isFinite(number));
+// console.log(Number.isInteger(number));
+
+
+/*******Arrays(12) */
+// let array = [10, 12, 14]
+// console.log(array)
+
+// let newArray = Array.from(array, val => val * 2) // create an array based on a certain array
+// console.log(newArray)
+
+// array.fill(100) // takes the older array and replaces each of the val by a specified one
+// console.log(array);
+
+// array.fill(50, 1, 2)
+// console.log(array);
+
+// console.log(array.find(val => val <= 50)) // stops after the first match
+
+// let arr = [1,2,3]
+// console.log(arr.copyWithin(1, 2))
+
+// let it = arr.entries()
+// for (const element of it) {
+//   console.log(element)
+// }
+
+
+
+
+
+
+
+
+
+
